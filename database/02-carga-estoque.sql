f1ee20303545a532783774678a18f7ec6d25ffe8
José Romualdo <joseromualdo@outlook.com>
2024-08-14 16:41:10 -0300

    feat: add scripts

--- database/02-carga-estoque.sql
+++ database/02-carga-estoque.sql
@@ -1,6 +1,6 @@
 -- Inserindo dados na tabela Estoque
 INSERT INTO Estoque (quantidade_disponivel) VALUES 
-(100),  -- Produto 1
+(1),  -- Produto 1
 (200),  -- Produto 2
 (150),  -- Produto 3
 (80),   -- Produto 4
